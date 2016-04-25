f7e0eabf3fceb7dfd7e8f28f5157da097c88bccb
S-Shimotori <490.frostybirds@gmail.com>
2016-04-25 20:54:04 +0900

    Answer #15

---
+++ Chapter2/15/15.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.argv[1])
+
+hightemp = open('../hightemp.txt')
+hightemp_data = hightemp.readlines()
+hightemp.close()
+
+output_python = open('output_python.txt', 'w')
+index = N if N < len(hightemp_data) else len(hightemp_data)
+output_python.write(''.join(hightemp_data[-index:]))
+output_python.close()
